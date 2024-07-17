01553f867039ee31a7f04bc4d413377af581035f
HjortOfCheliax <g.gkipatoudis@gmail.com>
2024-07-17 20:15:58 +0300

    Create morel User as a parent object for  Models Tutor and Student.

--- Homework/mainapp/models.py
+++ Homework/mainapp/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
 
+class User(models.Model):
+  user_dict = {
+    "Tutor": "Tutor",
+    "Student": "Student",
+  }
+
+  fname = models.CharField(max_length=100, blank=False, null=False)
+  sname = models.CharField(max_length=100, blank=False, null=False)
+  email = models.EmailField(max_length=254, blank=False, null=False)
+  user_type = models.CharField(max_length=100, choice=user_dict)
+  
+  def __str__(self):
+      return f"{self.fname} {self.sname}"
+  
+  class Meta:
+        abstract = True
+
 class Tutor(models.Model):
   
   subject_dict = {
@@ -9,21 +26,17 @@ class Tutor(models.Model):
     "Physics": "Physics",
     "Chemistry": "Chemistry",
   }
-
-  fname = models.CharField(max_length=100, blank=False, null=False)
-  sname = models.CharField(max_length=100, blank=False, null=False)
-  email = models.EmailField(max_length=254, blank=False, null=False)
-  subjects = models.Charfield(max_length=1, choices=subject_dict)
+  
+  user = models.ForeignKey(User, on_delete=CASCADE)
+  subjects = models.CharField(max_length=100, choices=subject_dict)
   
   def __str__(self):
       return f"{self.fname} {self.sname}" 
   
   
-class Student(models.Model):
+class Student(User):
   
-  fname = models.CharField(max_length=100, blank=False, null=False)
-  sname = models.CharField(max_length=100, blank=False, null=False)
-  email = models.EmailField(max_length=254, blank=False, null=False)
+  user = models.ForeignKey(User, on_delete=CASCADE)
   
   def __str__(self):
       return f"{self.fname} {self.sname}" 
--- Homework/mainapp/views.py
+++ Homework/mainapp/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Welcome to HomeWork!")
+  return render(request, "mainapp/index.html")
+
+def homepage(request):
+  return render(request, "mainapp/homepage.html")
+
+def profile(request):
+  pass
