68d3d5a2d9118ae1aca0b626be920569912e36f4
HjortOfCheliax <g.gkipatoudis@gmail.com>
2024-07-17 19:38:37 +0300

    Create Tutor and Student model

--- Homework/mainapp/models.py
+++ Homework/mainapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Tutor(models.Model):
+  
+  subject_dict = {
+    "Algebra": "Algebra",
+    "Geometry": "Geometry",
+    "Physics": "Physics",
+    "Chemistry": "Chemistry",
+  }
+
+  fname = models.CharField(max_length=100, blank=False, null=False)
+  sname = models.CharField(max_length=100, blank=False, null=False)
+  email = models.EmailField(max_length=254, blank=False, null=False)
+  subjects = models.Charfield(max_length=1, choices=subject_dict)
+  
+  def __str__(self):
+      return f"{self.fname} {self.sname}" 
+  
+  
+class Student(models.Model):
+  
+  fname = models.CharField(max_length=100, blank=False, null=False)
+  sname = models.CharField(max_length=100, blank=False, null=False)
+  email = models.EmailField(max_length=254, blank=False, null=False)
+  
+  def __str__(self):
+      return f"{self.fname} {self.sname}" 
+  
+  
